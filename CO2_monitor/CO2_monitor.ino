78dbdf55551f188d1ab4a67ea16bdf118fc9f2b7
James <cellojaems@gmail.com>
2018-12-20 16:43:53 -0700

    added i2c liquid crystal display library

--- CO2_monitor/CO2_monitor.ino
+++ CO2_monitor/CO2_monitor.ino
@@ -1,7 +1,10 @@
 #include <Arduino_FreeRTOS.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 #include "fan.h"
 #include "solenoid.h"
 #include "CO2_Sensor.h"
+
 
 float CO2_target = 0;
 float CO2_level = 0;
@@ -21,6 +24,9 @@ void setup()
   //setting up Interrupts
   attachInterrupt(0, changeCO2_target, CHANGE);
   attachInterrupt(1, setFans, CHANGE);
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
   
   //priotities
   int manageCO2_priority = 1;
@@ -75,18 +81,19 @@ void setFans(void)
  */
 void writeToCloud(void *pvParameters)
 {
+  bool toggle = false;
+  
   //runs when function is called for the first time.
-  int timeDelay = 1000; //in ms
-  pinMode(LED_BUILTIN, OUTPUT);
+  int timeDelay = 2000; //in ms
+  pinMode(12, OUTPUT);
   //runs forever
   for(;;)
   {
     noInterrupts();
     //write to cloud here
-    digitalWrite(LED_BUILTIN, HIGH);
+    toggle = !toggle;
+    digitalWrite(12, toggle);
     interrupts();
-    vTaskDelay(timeDelay / portTICK_PERIOD_MS);
-    digitalWrite(LED_BUILTIN, LOW);
     vTaskDelay(timeDelay / portTICK_PERIOD_MS);
   }
 }
@@ -96,12 +103,16 @@ void writeToCloud(void *pvParameters)
  */
 void writeToFile(void *pvParameters)
 {
+  bool toggle = false;
+  pinMode(5, OUTPUT);
   //runs when function is called for the first time.
   int timeDelay = 1000; //in ms
 
   //runs forever
   for(;;)
   {
+    toggle = !toggle;
+    digitalWrite(5, toggle);
     noInterrupts();
     //write to file here
     interrupts();
@@ -114,12 +125,16 @@ void writeToFile(void *pvParameters)
  */
 void displayLCD(void *pvParameters)
 {
+  bool toggle = false;
+  pinMode(11, OUTPUT);
   //runs when function is called for the first time.
-  int timeDelay = 1000; //in ms
+  int timeDelay = 500; //in ms
 
   //runs forever
   for(;;)
   {
+    toggle = !toggle;
+    digitalWrite(11, toggle);
     
     // divide by "portTICK_PERIOD_MS" to convert to seconds
     vTaskDelay(timeDelay / portTICK_PERIOD_MS); 
@@ -131,15 +146,19 @@ void displayLCD(void *pvParameters)
  */
 void readCO2_sensor(void *pvParameters)
 {
+  bool toggle = false;
+  pinMode(9, OUTPUT);
   //runs when function is called for the first time.
   int timeDelay = 200; //in ms
-  SoftwareSerial *temp(1, 2);
-  CO2_Sensor co2(temp, baud);
+//  SoftwareSerial *temp(1, 2);
+//  CO2_Sensor co2(temp, baud);
 
   //runs forever
   for(;;)
   {
-    co2.read();
+    toggle = !toggle;
+    digitalWrite(9, toggle);
+//    co2.read();
     vTaskDelay(timeDelay / portTICK_PERIOD_MS);
   }
 }
@@ -149,14 +168,17 @@ void readCO2_sensor(void *pvParameters)
  */
 void manageCO2_levels(void *pvParameters)
 {
+  bool toggle = false;
+  pinMode(7, OUTPUT);
   //runs when function is called for the first time.
   Solenoid solenoid;
-  int timeDelay = 10; //in ms
+  int timeDelay = 300; //in ms
 
   //runs forever.
   for(;;)
   {
-    
+    toggle = !toggle;
+    digitalWrite(7, toggle);
     vTaskDelay(timeDelay / portTICK_PERIOD_MS);
   }
 }
