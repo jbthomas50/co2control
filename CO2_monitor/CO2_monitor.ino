c9ada83f16eae2e783ec7620a0f779f9d93d5ca8
James <cellojaems@gmail.com>
2018-12-21 17:10:31 -0700

    played with the code. Too much memorygit pull!

--- CO2_monitor/CO2_monitor.ino
+++ CO2_monitor/CO2_monitor.ino
@@ -6,11 +6,10 @@
 #include "CO2_Sensor.h"
 
 
-float CO2_target = 0;
-float CO2_level = 0;
+volatile float CO2_target = 0;
+volatile float CO2_level = 0;
 uint32_t baud = 9600; //global baud-rate
-Fan fans[5] = {Fan(0), Fan(1), Fan(2), Fan(3), Fan(4)};
-LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
+Fan fans[4] = {Fan(0), Fan(1), Fan(2), Fan(3)};
 
 void setFans(void);
 void changeCO2_target(void);
@@ -25,10 +24,10 @@ void setup()
   //setting up Interrupts
 //  attachInterrupt(0, changeCO2_target, CHANGE);
 //  attachInterrupt(1, setFans, CHANGE);
-  lcd.begin(16, 2);
-  lcd.print("ACT:             ");
-  lcd.setCursor(0,1);
-  lcd.print("SET:             ");
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
   
   //priotities
   int manageCO2_priority = 1;
@@ -37,21 +36,22 @@ void setup()
   int writeToCloud_priority = 4;
   int displayLCD_priority = 5;
 
-
   xTaskCreate(displayLCD, "Displaying to LCD", 128,
               NULL, displayLCD_priority, NULL);
-  xTaskCreate(writeToFile, "Writting to file", 128, 
-              NULL, writeToFile_priority, NULL);
-  xTaskCreate(readCO2_sensor, "Reading CO2 sensor", 128,
-              NULL, readCO2_priority, NULL);
+//  xTaskCreate(saveData, "Writting to file", 128, 
+//              NULL, writeToFile_priority, NULL);
   xTaskCreate(manageCO2_levels, "Managing CO2 levels", 128,
               NULL, manageCO2_priority, NULL);
-  xTaskCreate(writeToCloud, "Writting to Google Sheets", 128,
-              NULL, writeToCloud_priority, NULL);
+
+//  xTaskCreate(readCO2_sensor, "Reading CO2 sensor", 128,
+//              NULL, readCO2_priority, NULL);
+//  xTaskCreate(writeToCloud, "Writting to Google Sheets", 128,
+//              NULL, writeToCloud_priority, NULL);
 }
 
 void loop() 
 {
+//  Serial.println("Bad loop!!");
   // Empty. Things are done in Tasks.
 }
 
@@ -68,9 +68,8 @@ void changeCO2_target(void)
  */
 void setFans(void)
 {
-  int numFans = map(analogRead(A0), 0, 1023, 0, 5);
-  
-  for(int i = 0; i < 5; i++)
+  int numFans = map(analogRead(A0), 0, 1023, 0, 4);
+  for(int i = 0; i < 4; i++)
   {
     if (i < numFans)
       fans[i].on();
@@ -84,18 +83,15 @@ void setFans(void)
  */
 void writeToCloud(void *pvParameters)
 {
-  bool toggle = false;
+  (void) pvParameters;
   
   //runs when function is called for the first time.
   int timeDelay = 2000; //in ms
-  pinMode(12, OUTPUT);
   //runs forever
   for(;;)
   {
     noInterrupts();
     //write to cloud here
-    toggle = !toggle;
-    digitalWrite(12, toggle);
     interrupts();
     vTaskDelay(timeDelay / portTICK_PERIOD_MS);
   }
@@ -104,18 +100,15 @@ void writeToCloud(void *pvParameters)
 /**
  * Writes to SD card
  */
-void writeToFile(void *pvParameters)
+void saveData(void *pvParameters)
 {
-  bool toggle = false;
-  pinMode(5, OUTPUT);
+  (void) pvParameters;
   //runs when function is called for the first time.
   int timeDelay = 1000; //in ms
 
   //runs forever
   for(;;)
   {
-    toggle = !toggle;
-    digitalWrite(5, toggle);
     noInterrupts();
     //write to file here
     interrupts();
@@ -128,18 +121,44 @@ void writeToFile(void *pvParameters)
  */
 void displayLCD(void *pvParameters)
 {
-  bool toggle = false;
-  pinMode(11, OUTPUT);
+  (void) pvParameters;
+  #define bottomPot A0
+  #define topPot A1
+
+  pinMode(bottomPot, INPUT);
+  pinMode(topPot, INPUT);
   //runs when function is called for the first time.
   int timeDelay = 500; //in ms
+  LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
+  lcd.begin(16, 2);
+  lcd.print("ACT:             ");
+  lcd.setCursor(0,1);
+  lcd.print("SET:             ");
 
   //runs forever
   for(;;)
   {
-    toggle = !toggle;
-    digitalWrite(11, toggle);
+   lcd.home ();
+   lcd.clear();
 
-    
+   long temp = map(analogRead(bottomPot), 0, 1023, 0, 101);
+   long temp1 = map(analogRead(topPot), 0, 1023, 1, 101);
+   temp *= 100;
+   temp1 *= 10000;
+
+   lcd.print("ACT: ");
+   lcd.print("XXXXXXX|fan");
+   lcd.setCursor(0,1);
+   lcd.print("SET: ");
+//   lcd.print(2);
+//   lcd.setCursor(5, 1);
+//   String num = (String) (temp + temp1);
+//   for(int i = 0; i < 6 - num.length(); i++)
+//   {
+//       lcd.print(" ");
+//   }
+   lcd.print(temp + temp1);
+   lcd.print("|  2");
     
     // divide by "portTICK_PERIOD_MS" to convert to seconds
     vTaskDelay(timeDelay / portTICK_PERIOD_MS); 
@@ -151,19 +170,13 @@ void displayLCD(void *pvParameters)
  */
 void readCO2_sensor(void *pvParameters)
 {
-  bool toggle = false;
-  pinMode(9, OUTPUT);
+  (void) pvParameters;
   //runs when function is called for the first time.
   int timeDelay = 200; //in ms
-//  SoftwareSerial *temp(1, 2);
-//  CO2_Sensor co2(temp, baud);
 
   //runs forever
   for(;;)
   {
-    toggle = !toggle;
-    digitalWrite(9, toggle);
-//    co2.read();
     vTaskDelay(timeDelay / portTICK_PERIOD_MS);
   }
 }
@@ -174,8 +187,7 @@ void readCO2_sensor(void *pvParameters)
  */
 void manageCO2_levels(void *pvParameters)
 {
-  bool toggle = false;
-  pinMode(7, OUTPUT);
+  (void) pvParameters;
   //runs when function is called for the first time.
   Solenoid solenoid;
   int timeDelay = 300; //in ms
@@ -183,8 +195,6 @@ void manageCO2_levels(void *pvParameters)
   //runs forever.
   for(;;)
   {
-    toggle = !toggle;
-    digitalWrite(7, toggle);
     setFans();
     vTaskDelay(timeDelay / portTICK_PERIOD_MS);
   }
