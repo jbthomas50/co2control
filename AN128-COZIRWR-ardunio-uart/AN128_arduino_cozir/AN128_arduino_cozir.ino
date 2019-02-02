257e9de74e954bf1c473d3577d215da3edb4222d
James <cellojaems@gmail.com>
2019-02-02 16:19:57 -0700

    changed some sensor code

--- AN128-COZIRWR-ardunio-uart/AN128_arduino_cozir/AN128_arduino_cozir.ino
+++ AN128-COZIRWR-ardunio-uart/AN128_arduino_cozir/AN128_arduino_cozir.ino
@@ -20,10 +20,13 @@ AN128_ardunio_cozir CO2 Demonstration code 11/29/2017 Runs on Ardunio UNO, MEGA 
 SoftwareSerial mySerial(12, 13); // RX, TX pins on Ardunio
 
 int co2 =0;
-double multiplier = 1;// 1 for 2% =20000 PPM, 10 for 20% = 200,000 PPM
+int humidity = 0;
+int temp = 0;
+double multiplier = 100;// 1 for 2% =20000 PPM, 10 for 20% = 200,000 PPM
 uint8_t buffer[25];
-uint8_t ind =0;
-uint8_t index =0;
+uint8_t ind = 0;
+uint8_t index = 0;
+#define bufferMax 7
 
 int fill_buffer();  // function prototypes here
 int format_output();
@@ -34,55 +37,96 @@ void setup() {
   Serial.println("             AN128 Ardunio to Cozir CO2 Sensor - Demonstration code 11/29/2017\n\n"); 
   mySerial.begin(9600); // Start serial communications with sensor
   mySerial.println("K 0");  // Set Command mode
-  mySerial.println("M 6"); // send Mode for Z and z outputs
-  // "Z xxxxx z xxxxx" (CO2 filtered and unfiltered)
-
-  mySerial.println("K 1");  // set streaming mode
-  Serial.println("finished setup");
+  mySerial.println("Z");// send Mode for H, T, and Z outputs
+  // "H xxxxx T xxxxx Z xxxxx" (humidity, temperature, and filtered CO2 level)
+  mySerial.println("K 2");  // set polling mode
 }
 
 void loop() {
+  for(int i = 0; i < bufferMax; i++)
+  {
+    buffer[i] = ' ';
+  }
+  mySerial.println("Z");// send Mode for H, T, and Z outputs
+  // "H xxxxx T xxxxx Z xxxxx" (humidity, temperature, and filtered CO2 level)
   fill_buffer();  // function call that reads CO2 sensor and fills buffer
  
-    Serial.print("Buffer contains: ");
-  for(int j=0; j<ind; j++)Serial.print(buffer[j],HEX);
- index = 0;
- format_output();
-  Serial.print(" Raw PPM        ");
- 
-  index = 8;  // In ASCII buffer, filtered value is offset from raw by 8 bytes
-  format_output();
-  Serial.println(" Filtered PPM\n\n");
+  Serial.print("Buffer contains: ");
+  for(int j=0; j<ind; j++)
+  {
+    Serial.print((char)buffer[j]);
+  }
+  Serial.print("\n");
+
+//  index = 16;
+//  Serial.print("\tHumidity");
+//  format_output_humidity();
+//
+//  index = 8;  // In ASCII buffer, filtered value is offset from raw by 8 bytes
+//  Serial.print("\tTemperature");
+//  format_output_temp();
+
+  index = 0;
+  Serial.print("\tFiltered PPM");
+  format_output_co2();
+
+  Serial.println("\n\n");
+  delay(5000);
 }
 
-int fill_buffer(void){        
-  
-
-// Fill buffer with sensor ascii data
-ind = 0;
-Serial.println("filling");
-while(buffer[ind-1] != 0x0A){  // Read sensor and fill buffer up to 0XA = CR
-  if(mySerial.available()){
-    buffer[ind] = mySerial.read();
-    ind++;        
+int fill_buffer(void)
+{        
+  // Fill buffer with sensor ascii data
+  ind = 0;
+  while(buffer[ind-1] != 0x0A)
+  {  // Read sensor and fill buffer up to 0XA = CR
+    if(mySerial.available())
+    {
+      buffer[ind] = mySerial.read();
+      ind++;   
     } 
   }        
-  Serial.println("Serial is available");
   // buffer() now filled with sensor ascii data
-  // ind contains the number of characters loaded into buffer up to 0xA =  CR
+//   ind contains the number of characters loaded into buffer up to 0xA =  CR
   ind = ind -2; // decrement buffer to exactly match last numerical character
-  }
+}
 
- int format_output(void){ // read buffer, extract 6 ASCII chars, convert to PPM and print
-   co2 = buffer[15-index]-0x30;
-  co2 = co2+((buffer[14-index]-0x30)*10);
-  co2 +=(buffer[13-index]-0x30)*100;
-  co2 +=(buffer[12-index]-0x30)*1000;
-  co2 +=(buffer[11-index]-0x30)*10000;
+int format_output_co2(void){ // read buffer, extract 6 ASCII chars, convert to PPM and print
+  co2 = buffer[bufferMax-index++]-0x30;
+  co2 += (buffer[bufferMax-index++]-0x30)*10;
+  co2 += (buffer[bufferMax-index++]-0x30)*100;
+  co2 += (buffer[bufferMax-index++]-0x30)*1000;
+  co2 += (buffer[bufferMax-index++]-0x30)*10000;
   Serial.print("\n CO2 = ");
-  Serial.print(co2*multiplier,0);
- Serial.print(" PPM,");
-    Serial.print("\n");
-  delay(200);
+  Serial.print(co2*multiplier, 0);
+  Serial.print(" PPM");
+  Serial.print("\n");
+ }
+
+int format_output_humidity(void){ // read buffer, extract 6 ASCII chars, convert to PPM and print
+  humidity = buffer[bufferMax-index++];
+  humidity += (buffer[bufferMax-index++])*10;
+  humidity += (buffer[bufferMax-index++])*100;
+  humidity += (buffer[bufferMax-index++])*1000;
+  humidity += (buffer[bufferMax-index++])*10000;
+  humidity /= 10;
+  Serial.print("\n Humidity = ");
+  Serial.print(humidity, DEC);
+  Serial.print("%");
+  Serial.print("\n");
+ }
+
+int format_output_temp(void){ // read buffer, extract 6 ASCII chars, convert to PPM and print
+  temp = (int)buffer[bufferMax-index++];
+  temp += (int)(buffer[bufferMax-index++])*10;
+  temp += (int)(buffer[bufferMax-index++])*100;
+  temp += (int)(buffer[bufferMax-index++])*1000;
+  temp += (int)(buffer[bufferMax-index++])*10000;
+  temp -= 1000;
+  temp /= 10;
+  Serial.print("\n Temperature = ");
+  Serial.print(temp, DEC);
+  Serial.print(" C");
+  Serial.print("\n");
  }
 
